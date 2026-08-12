d30d521c49064c732f8f5be296746638414a6e01
Lazy-Shadow <thederpsmc3@gmail.com>
2026-08-12 21:17:41 +0800

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/services" element={<ServicesPage />} />
+      <Route path="/contact-us" element={<ContactUsPage />} />
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,8 @@
+
+export default function AboutPage() {
+    return (
+        <h1>This is the <span className="text-blue-500">ABOUT</span> Page.</h1>
+    )
+}
+
+// bukas ulit? or tuloy? 
