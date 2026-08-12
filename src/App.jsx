eb4effc1cdb5c76a972adeb8305e2a588ff61f8e
Lazy-Shadow <thederpsmc3@gmail.com>
2026-08-12 21:10:54 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+      <Header />
+
+      <main className='flex flex-1'>
+
+      </main>
+      
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <header>
+            <nav className="flex flex-col justify-content items-center">
+                <a id='brand' className="text-2xl font-bold" href="/">Blank CO.</a>
+            </nav>
+
+        </header>
+    )
+}
